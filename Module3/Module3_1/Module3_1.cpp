e24bb403307d813f9ad3d541fc11fb7f353b7f59
Mikalai96 <nasonov.962015@gmail.com>
2024-11-25 14:32:25 +0300

    Раскоментил код

--- Module3/Module3_1/Module3_1.cpp
+++ Module3/Module3_1/Module3_1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int a = 1, b = 1;
-    /*while (a<=10)
+    while (a<=10)
     {
         cout << a * b << "\t";
         if (b < 10)
@@ -18,25 +18,26 @@ int main()
             a++;
             b = 1;
         }
-    }*/
-
-    /*for (int a = 1; a <= 10; a++)
+    }
+ cout << "\n";
+    for (int a = 1; a <= 10; a++)
     {
         for (int b = 1; b <= 10; b++)
         {
             cout << a * b << "\t";
         }
         cout << "\n";
-    }*/
-    //a = 0;
-    //do
-    //{        
-    //    //cout << a * b << "\t";      
-    //    a++;
-    //    for (int b = 1; b <= 10; b++)
-    //    {
-    //        cout << a * b << "\t";
-    //    }
-    //    cout << "\n";
-    //} while (a <= 10);     
+    }
+cout << "\n";
+    a = 0;
+    do
+    {        
+        //cout << a * b << "\t";      
+        a++;
+        for (int b = 1; b <= 10; b++)
+        {
+            cout << a * b << "\t";
+        }
+        cout << "\n";
+    } while (a < 10);     
 }
