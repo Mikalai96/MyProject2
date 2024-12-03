37b6e3bd3d49071cb35905eb8eb3872d0548c13b
Mikalai96 <nasonov.962015@gmail.com>
2024-12-03 19:19:38 +0300

    Модуль 4

---
+++ Module4/Module4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Module 4\n";
+
+    std::cout << "Enter number\n";
+
+    int nmbr, shift;
+
+    std::cin >> nmbr;
+
+    if ( nmbr%2==0 && nmbr>0 ) // Использована конструкция if-else
+    {
+        shift = nmbr << 2;
+        std::cout << "Result shift left: " << shift << "\n";
+    }
+    else
+    {
+        shift = nmbr >> 2;
+        std::cout << "Result shift right: " << shift << "\n";
+    }
+
+    std::cout << "Enter number\n"; // Использован условный тернарный оператор
+    
+    std::cin >> nmbr;
+
+    nmbr % 2 == 0 && nmbr > 0 ? std::cout << "Result shift left: " << (nmbr << 2) : 
+        std::cout << "Result shift right: " << (nmbr >> 2);
+}
