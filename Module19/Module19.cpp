dc0819f0d3b191ae1e0f9c5cbb6be281451329af
Mikalai96 <nasonov.962015@gmail.com>
2025-02-16 16:26:45 +0300

    Module19

---
+++ Module19/Module19.cpp
@@ -0,0 +1,28 @@
+﻿#include "Source.h"
+
+int main() {
+    // Генерация массива 
+    std::vector<int> data(100);
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+       for (int& num : data) {
+        num = std::rand() % 100 + 1;
+    }
+    printArray(data); //Вывод массива
+    
+    // Создание потоков
+    std::thread sumThread(calculateSum, std::ref(data));
+    std::thread maxThread(findMax, std::ref(data));
+    std::thread minThread(findMin, std::ref(data));
+
+    // Ожидание завершения потоков
+    sumThread.join();
+    maxThread.join();
+    minThread.join();
+
+    // Вывод результатов
+    std::cout << "Sum: " << sumResult << std::endl;
+    std::cout << "Max: " << maxResult << std::endl;
+    std::cout << "Min: " << minResult << std::endl;
+
+    return 0;
+}
---
+++ Module19/Source.cpp
@@ -0,0 +1,46 @@
+﻿#include "Source.h"
+// Переменные для хранения результатов
+ int sumResult = 0;
+ int maxResult = 0;
+ int minResult = 0;
+
+// Мьютекс для синхронизации доступа к результатам
+ std::mutex resultMutex;
+
+// Функция для вывода массива по 20 элементов в строке
+void printArray(const std::vector<int>& data) {
+    std::cout << "Array: " << std::endl;
+    for (size_t i = 0; i < data.size(); ++i) {
+        std::cout << data[i] << " ";
+        if ((i + 1) % 20 == 0) {
+            std::cout << std::endl;
+        }
+    }
+}
+
+// Функция для вычисления суммы элементов массива
+void calculateSum(const std::vector<int>& data) {
+    int localSum = 0;
+    for (int num : data) {
+        localSum += num;
+    }
+    // Синхронизация записи результата
+    std::lock_guard<std::mutex> lock(resultMutex);
+    sumResult = localSum;
+}
+
+// Функция для нахождения максимального элемента массива
+void findMax(const std::vector<int>& data) {
+    int localMax = *std::max_element(data.begin(), data.end());
+    // Синхронизация записи результата
+    std::lock_guard<std::mutex> lock(resultMutex);
+    maxResult = localMax;
+}
+
+// Функция для нахождения минимального элемента массива
+void findMin(const std::vector<int>& data) {
+    int localMin = *std::min_element(data.begin(), data.end());
+    // Синхронизация записи результата
+    std::lock_guard<std::mutex> lock(resultMutex);
+    minResult = localMin;
+}
