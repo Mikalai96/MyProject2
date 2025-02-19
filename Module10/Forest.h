6f5e11635b462ce6c554a510e035aecb7a239e8f
Mikalai96 <nasonov.962015@gmail.com>
2025-02-19 13:54:34 +0300

    Module10

---
+++ Module10/Forest.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <vector>
+#include <memory>
+#include "Tree.h"
+
+class Forest {
+protected:
+    std::vector<std::shared_ptr<Tree>> trees;
+
+public:
+    virtual ~Forest() = default;
+    virtual void addTree(std::shared_ptr<Tree> tree) = 0;
+    void wind() const;
+    size_t getTreesCount() const;
+};
+
+class DeciduousForest : public Forest {
+public:
+    void addTree(std::shared_ptr<Tree> tree) override;
+};
+
+class ConiferousForest : public Forest {
+public:
+    void addTree(std::shared_ptr<Tree> tree) override;
+};
---
+++ Module10/Module10.cpp
@@ -0,0 +1,34 @@
+﻿#include "Forest.h"
+#include"Tree.h"
+#include <cstdlib>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    DeciduousForest deciduousForest;
+    ConiferousForest coniferousForest;
+
+    // Создаем деревья
+    deciduousForest.addTree(std::make_shared<Oak>("Дуб1"));
+    deciduousForest.addTree(std::make_shared<Oak>("Дуб2"));
+    deciduousForest.addTree(std::make_shared<Birch>("Береза1"));
+
+    coniferousForest.addTree(std::make_shared<Pine>("Сосна1"));
+    coniferousForest.addTree(std::make_shared<Pine>("Сосна2"));
+
+    // Попытка добавить неправильный тип дерева
+    deciduousForest.addTree(std::make_shared<Pine>("Сосна3"));
+
+    std::cout << "\nШум лиственного леса:" << std::endl;
+    deciduousForest.wind();
+
+    std::cout << "\nШум хвойного леса:" << std::endl;
+    coniferousForest.wind();
+
+    std::cout << "\nСтатистика по деревьям:" << std::endl;
+    std::cout << "Количество дубов: " << Oak::getCount() << std::endl;
+    std::cout << "Количество берез: " << Birch::getCount() << std::endl;
+    std::cout << "Количество сосен: " << Pine::getCount() << std::endl;
+
+    return 0;
+}
